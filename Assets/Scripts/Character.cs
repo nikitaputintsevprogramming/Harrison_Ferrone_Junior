a8743e9c2cd9e5529719506595d807ee91de34c9
nikitaputintsevprogramming <nikitaputintsevprogramming@gmail.com>
2023-08-15 22:21:47 +0300

    5 глава ООП 142 страница

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character
+{
+    public string name;
+    public int exp = 0;
+}
---
+++ Assets/Scripts/LearningCurve.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+    void Start()
+    {
+        Character hero = new Character();
+        Debu
+    }
+
+    void Update()
+    {
+        
+    }
+}
