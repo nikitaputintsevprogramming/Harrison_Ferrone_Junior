3cc1919929cde6c98b6c0dfe94e06827dd9689de
nikitaputintsevprogramming <nikitaputintsevprogramming@gmail.com>
2023-08-16 11:56:53 +0300

    ǲонкретизируем детали персонажа

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Character
+{
+    public string name;
+    public int exp = 0;
+}
---
+++ Assets/Scripts/LearningCurve.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+    void Start()
+    {
+        Character hero = new Character();
+        Debug.LogFormat("Hero: {0} - {1} EXP", hero.name, hero.exp);
+    }
+
+    void Update()
+    {
+
+    }
+
+}
