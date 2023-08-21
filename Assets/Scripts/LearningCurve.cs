39297564b7dad850b506c00c63cf559447d6a291
nikitaputintsevprogramming <nikitaputintsevprogramming@gmail.com>
2023-08-21 11:11:40 +0300

    дошли до ООП

--- Assets/Scripts/LearningCurve.cs
+++ Assets/Scripts/LearningCurve.cs
@@ -9,13 +9,20 @@ public class LearningCurve : MonoBehaviour
     void Start()
     {
         Character hero = new Character();
+        Character hero2 = hero;
+        hero2.name = "Richard";
         hero.PrintStatsInfo();
+        hero2.PrintStatsInfo();
+        
 
         Character herione = new Character("Agatcha");
         herione.PrintStatsInfo();
 
         Weapon huntingBow = new Weapon("Hunting Bow", 105);
+        Weapon warBow = huntingBow;
+
         huntingBow.PrintWeaponInfo();
+        warBow.PrintWeaponInfo();
     }
 
     void Update()
